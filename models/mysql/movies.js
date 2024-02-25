0b0a5f684bb89871097318c71fa057423ad0c2b3
p-leo2 <leooel23m@gmail.com>
2024-02-25 08:07:13 -0400

    add eslint and pg(postgres driver)

---
+++ models/mysql/movies.js
@@ -0,0 +1,24 @@
+import { Client } from "pg";
+
+const client = new Client()
+await client.connect({
+    
+})
+export class MovieModel {
+    static async getAll({ genre }) {
+    }
+
+    static async getByid({ id }) {
+    }
+
+    static async create({ input }) {
+
+    }
+
+    static async delete({ id }) {
+   }
+
+    static async update({ id, input }) {
+
+    }
+}
