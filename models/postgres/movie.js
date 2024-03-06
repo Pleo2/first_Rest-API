68c66a7baea4f4361a174b9120e54525e6be833c
pleo2 <leooel23m@gmail.com>
2024-03-06 08:43:52 -0400

    add create movie and update genre_movie

--- models/postgres/movie.js
+++ models/postgres/movie.js
@@ -5,7 +5,7 @@ export class MovieModel {
         if (genre) {
             const lowerCaseGenre = genre.toLowerCase()
             const { rows } = await pg.query(
-                'SELECT id, name FROM genre WHERE LOWER(name) = $1',
+                'SELECT id, name FROM genre WHERE LOWER(name) = $1;',
                 [lowerCaseGenre],
             )
             if (rows[0].id) {
@@ -28,25 +28,73 @@ export class MovieModel {
 
     static async getByid({ id }) {
         const { rows: movie } = await pg.query(
-        'SELECT title, year, director, duraction, poster, rate, id FROM movie WHERE id = $1;',
-        [id]
-    )
+            'SELECT title, year, director, duraction, poster, rate, id FROM movie WHERE id = $1;',
+            [id],
+        )
         if (movie.length === 0) return null
         return movie[0]
     }
 
     static async create({ input }) {
-        
+        // create a filter que inpida crear una pelicula varias veces con el mismo nombre 
+        const {
+            genre: genreInput, // genre is an array
+            title,
+            year,
+            duration,
+            director,
+            rate,
+            poster,
+        } = input
+
+        // todo: crear la conexión de genre
+
+        // crypto.randomUUID()
+        const {rows: result} = await pg.query('SELECT gen_random_uuid() uuid;')
+        const uuid = result[0].uuid
+        console.log(uuid)
+        try {
+            await pg.query(
+                'INSERT INTO movie (id, title, year, director, duraction, poster, rate) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+                [uuid, title, year, director, duration, poster, rate],
+            )
+
+            if (genreInput.length > 0) {
+                genreInput.forEach(async element => {
+                    const {rows: result} = await pg.query('SELECT id FROM genre WHERE name = $1', [element])
+                    const idGenre = result[0].id
+                    console.log(typeof(idGenre))
+
+                    if (typeof(idGenre) === 'number') {
+                        await pg.query('INSERT INTO movie_genres (movie_id, genre_id) Values ($1,$2)', [uuid, idGenre])
+                    }
+                });
+            }
+        } catch (e) {
+            // throw new Error('Error creating movie')
+            console.log(e)
+            // enviar la traza a un servicio interno
+            // sendLog(e)
+        }
+
+        const { rows: movie } = await pg.query(
+            'SELECT title, year, director, duraction, poster, rate, id FROM movie WHERE id = $1;',
+            [uuid],
+        )
+        if (movie.length === 0) return null
+        return movie[0]
     }
 
     static async delete({ id }) {
         const { rows: movie } = await pg.query(
             'DELETE FROM movie WHERE id = $1;',
-            [id]
+            [id],
         )
-            if (movie.length === 0) return null
-            return movie[0]
+        if (movie.length === 0) return null
+        return movie[0]
     }
 
-    static async update({ id, input }) {}
+    static async update({ id, input }) {
+        // falta crear el update
+    }
 }
