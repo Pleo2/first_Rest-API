bfccf06e244c40717d2cbd6b19545676b207cc35
pleo2 <leooel23m@gmail.com>
2024-03-06 06:58:27 -0400

    add get by id and filter movies by genre

--- models/postgres/movie.js
+++ models/postgres/movie.js
@@ -1,26 +1,45 @@
-import pg from './pg.js'
+import { pg } from './pg.js'
 
 export class MovieModel {
     static async getAll({ genre }) {
-        try {
-            await pg.query('SELECT * FROM movie;')
-
-        } catch (error) {
-            console.log(error)
+        if (genre) {
+            const lowerCaseGenre = genre.toLowerCase()
+            const { rows } = await pg.query(
+                'SELECT id, name FROM genre WHERE LOWER(name) = $1',
+                [lowerCaseGenre],
+            )
+            if (rows[0].id) {
+                const { rows: filterMovies } = await pg.query(
+                    'SELECT id, year, duraction, poster, title, director, rate  FROM movie INNER JOIN movie_genres ON movie_id = movie.id WHERE genre_id = $1;',
+                    [rows[0].id],
+                )
+                console.log(filterMovies)
+                return filterMovies
+            } else {
+                return 'This id does not exist '
+            }
+        } else {
+            const { rows: movies } = await pg.query(
+                'SELECT id, year, duraction,poster,title,director,rate FROM movie;',
+            )
+            return movies
         }
     }
 
     static async getByid({ id }) {
+        const { rows: movie } = await pg.query(
+        'SELECT title, year, director, duraction, poster, rate, id FROM movie WHERE id = $1;',
+        [id]
+    )
+        if (movie.length === 0) return null
+        return movie[0]
     }
 
     static async create({ input }) {
-
+        
     }
 
-    static async delete({ id }) {
-   }
+    static async delete({ id }) {}
 
-    static async update({ id, input }) {
-
-    }
+    static async update({ id, input }) {}
 }
